061adc11754e13d50bffdd3757c1531b63ee4f2a
Yogitha <yogithaprushothaman@gmail.com>
2026-06-17 21:39:19 +0530

    Added product details API

--- main.py
+++ main.py
@@ -11,28 +11,46 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+products = [
+    {
+        "id": 1,
+        "name": "Atomic Habits",
+        "price": 499,
+        "author": "James Clear",
+    },
+    {
+        "id": 2,
+        "name": "Rich Dad Poor Dad",
+        "price": 399,
+        "author": "Robert Kiyosaki",
+    },
+    {
+        "id": 3,
+        "name": "Think and Grow Rich",
+        "price": 299,
+        "author": "Napoleon Hill",
+    },
+]
+
+
 @app.get("/")
 def home():
     return {
         "message": "Welcome to Discoverly Backend!"
     }
 
+
 @app.get("/products")
 def get_products():
-    return [
-        {
-            "id": 1,
-            "name": "Atomic Habits",
-            "price": 499
-        },
-        {
-            "id": 2,
-            "name": "Rich Dad Poor Dad",
-            "price": 399
-        },
-        {
-            "id": 3,
-            "name": "Think and Grow Rich",
-            "price": 299
-        }
-    ]
+    return products
+
+
+@app.get("/products/{product_id}")
+def get_product(product_id: int):
+    for product in products:
+        if product["id"] == product_id:
+            return product
+
+    return {
+        "message": "Product not found"
+    }
